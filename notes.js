7c12c4e6221224f996437d3effb318a6e8d511d3
Nicole Remy <nicoleremy95@gmail.com>
2020-11-21 14:38:35 -0800

    examples and rules

---
+++ notes.js
@@ -0,0 +1,13 @@
+//RULES 
+//Constants don't matter
+//arithmetic operations are constant
+//variable assignment is constant,
+//Accessing elements in an array (by index or object (by key)) is constant
+//In a loop, the complexity is the length of the loop times the complexity of what happens inside
+
+//EXAMPLES
+//O(2093) should be written as O(1)
+//O(2n) should be written as O(n)
+//O(n+10) should be written as O(n)
+//O(n^2 +8) should be written as O(n^2)
+//O(n^2 + 5n + 8) should be written as O(n^2)
