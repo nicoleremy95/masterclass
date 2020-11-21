dc62bd37867fd5943d80d6b620ef51860072ef59
Nicole Remy <nicoleremy95@gmail.com>
2020-11-21 14:27:54 -0800

    big o analysis example

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//BIG O, 
+//BIG O, f(n)=n,  O(n)
 //1 addition in a loop n times (n additions), n assignments as you do total +=, i++ scales with n, total = 0 is one assingment, i = 1 is 1 assignment, i <=n is n comparisons
 function addUpTo(n){
     let total = 0;
@@ -9,9 +9,25 @@ function addUpTo(n){
 }
 console.log(addUpTo(6))
 
-//BIG O
+//BIG O, f(n)=1, O(1)
 //1 multiplication, 1 addition, 1 division = 3 operations, this will always be the case regardless of the size of n
 function addUpTo(n){
     return n * (n+1) / 2
 }
 console.log(addUpTo(6))
+
+
+//BIG O, f(n)= n, O(n)         
+function countUpAndDown(n) {
+    console.log("Going up!");
+    for (var i = 0; i < n; i++) {
+      console.log(i);
+    }
+    console.log("At the top!\nGoing down...");
+    for (var j = n - 1; j >= 0; j--) {
+      console.log(j);
+    }
+    console.log("Back down. Bye!");
+  }
+
+  
