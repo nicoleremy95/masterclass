6afd4a7262575adc13c98fcd4f763bb11a7a96c0
Nicole Remy <nicoleremy95@gmail.com>
2020-11-23 10:22:44 -0800

    space complexity example

--- index.js
+++ index.js
@@ -83,3 +83,14 @@ function subtotals(array) {
     }
     return subtotalArray;
 }
+
+//BIG 0, O(n)
+function onlyElementsAtEvenIndex(array) {
+    var newArray = Array(Math.ceil(array.length / 2));
+    for (var i = 0; i < array.length; i++) {
+        if (i % 2 === 0) {
+            newArray[i / 2] = array[i];
+        }
+    }
+    return newArray;
+}
