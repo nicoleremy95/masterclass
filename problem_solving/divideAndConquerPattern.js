a09bc40c0350a232e7df3abbd0a912f4d00d822d
Nicole Remy <nicoleremy95@gmail.com>
2020-12-03 17:09:40 -0800

    data structures in relation to classes example and notes

---
+++ problem_solving/divideAndConquerPattern.js
@@ -0,0 +1,5 @@
+//DIVIDE AND CONQUER PATTERN 
+
+//Given a sorted array of integers, write a function called search that accepts a value and returns the index where the value passed to the function is located. if the value is not found, return -1
+
+//approach is similar to binary search pattern
