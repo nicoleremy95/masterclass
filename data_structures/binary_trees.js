1e90088c4ebaf1b0f12b43b29d3a637c00eff2c4
Nicole Remy <nicoleremy95@gmail.com>
2020-12-06 09:10:53 -0800

    binary search trees methods

--- data_structures/binary_trees.js
+++ data_structures/binary_trees.js
@@ -42,6 +42,85 @@ class binarySearchTree{
             
         }
     }
+
+    find(value){
+        if(this.root === null) return false;
+        var current = this.root,
+            found = false;
+        while(current && !found){
+            if(value < current.value){
+                current = current.left;
+            } else if(value > current.value){
+                current = current.right;
+            } else {
+                found = true;
+            }
+        }
+        if(!found) return undefined;
+        return current;
+    }
+    contains(value){
+        if(this.root === null) return false;
+        var current = this.root,
+            found = false;
+        while(current && !found){
+            if(value < current.value){
+                current = current.left;
+            } else if(value > current.value){
+                current = current.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    BreadthFirstSearch(){
+        //visit the node and then the children of the node from left to right, and then the next level of children from left to right and so on
+        var data = [];
+        var queue = [];
+        var newNode = this.root;
+        queue.push(this.root);
+        while(queue.length > 0){
+            newNode = queue.shift();
+            data.push(newNode.val)
+            if(newNode.left){
+                queue.push(newNode.left);
+            }
+            if(newNode.right){
+                queue.push(newNode.right);
+            }
+        }
+        return data;
+    }
+
+    DFSPreOrder(){
+        var data = [];
+
+        function traverse(node){
+            data.push(node.val);
+            //visit the root,then the entire left side and then the entire right side
+            if(node.left){
+                traverse(node.left);
+            }
+            if(node.right){
+                traverse(node.right)
+            }
+        }
+        traverse(this.root);
+        return data;
+    }
+    DFSPostOrder(){
+        let data = [];
+        function traverse(node){
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+            data.push(node.val);
+        }
+        traverse(this.root)
+        return data;
+    }
+    //go to the left and look at the last children first, and then go up to the parent. once the left side is done do this on the right, and then check the root last
 }
 
 var tree = new binarySearchTree();
